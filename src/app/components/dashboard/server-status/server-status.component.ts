095e100b9b8e3a04bd35cac943d8ee7cb3669543
Luis Polio <guspolio@outlook.es>
2024-08-02 11:08:50 -0600

    Se comienza a implementar el Lifecycle component

--- src/app/components/dashboard/server-status/server-status.component.ts
+++ src/app/components/dashboard/server-status/server-status.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
   standalone: true,
   imports: [],
   templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+  styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  //status:'online'| 'offline'| 'unknown'| = 'offline';
 
+  status = ['online', 'offline', 'unknown'];
+  currentStatus = this.status[Math.floor(Math.random() * this.status.length)];
+
+  constructor() {}
+
+  //En el siguiente método se comienza el lifecycle del componente, Angular se encarga de inicializar las entradas del componente.
+  //Es una practica recomendada usarlo para el trabajo de inicialización, y mantener el constructor delgado.
+  ngOnInit() {
+    setInterval(() => {
+      this.currentStatus =
+        this.status[Math.floor(Math.random() * this.status.length)];
+    }, 5000);
+  }
 }
