81ba60ee4e0f8acf80a3e4e45cb4dc7c688064ee
Luis Polio <guspolio@outlook.es>
2024-08-02 13:07:53 -0600

    Se introduce el uso de variable templates y de ViewChild tanto en decorator como en signal

--- src/app/components/dashboard/server-status/server-status.component.ts
+++ src/app/components/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnDestroy, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -9,18 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerStatusComponent implements OnInit {
   //status:'online'| 'offline'| 'unknown'| = 'offline';
-
+private destroyRef = inject(DestroyRef)
   status = ['online', 'offline', 'unknown'];
   currentStatus = this.status[Math.floor(Math.random() * this.status.length)];
-
+  //private interval?: ReturnType<typeof setInterval>
   constructor() {}
 
   //En el siguiente método se comienza el lifecycle del componente, Angular se encarga de inicializar las entradas del componente.
   //Es una practica recomendada usarlo para el trabajo de inicialización, y mantener el constructor delgado.
   ngOnInit() {
-    setInterval(() => {
+   const interval =  setInterval(() => {
       this.currentStatus =
         this.status[Math.floor(Math.random() * this.status.length)];
     }, 5000);
+
+
+    this.destroyRef.onDestroy(()=>{
+clearInterval(interval)
+    })
   }
+
+  //Destruye el componente despues de que desaparece de la pantalla, evita fugas de memoria.
+  /* ngOnDestroy(): void {
+    clearTimeout(this.interval)
+  } */
 }
--- src/app/components/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/components/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, ViewChildren, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
+  enteredTitle = '';
+  enteredRequest = '';
+  //Alternativa para acceder al elemento, usando decorators, en el argumento que recibe va ya sea el nombre de la instancia del componente, la clase o la template variable,
+//@ViewChild('form') form?: ElementRef<HTMLFormElement>;
+//disponible depues de angular 17.3
+private form = viewChild.required<ElementRef<HTMLFormElement>> ('form')
 
+onSubmit(title: string,) {
+    console.log('submit');
+    this.enteredTitle = title;
+    console.dir(this.enteredTitle);
+    console.log(this.enteredRequest);
+    console.log(this.form);
+ console.log(this.form()?.nativeElement.reset());
+ 
+    
+
+  }
 }
